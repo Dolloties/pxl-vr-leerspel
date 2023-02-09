d4fa81284052e696f060ff563631b3e20cf08835
Dolloties <101804337+Dolloties@users.noreply.github.com>
2023-02-09 17:27:45 +0100

    1.0

---
+++ testkin1.gd
@@ -0,0 +1,23 @@
+
+
+extends KinematicBody
+
+var velocity = Vector3(0, 0, 10) # move 10 units per second along the Z axis
+
+func _physics_process(delta):
+	move_and_slide(velocity)
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
