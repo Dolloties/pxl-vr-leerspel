1a6b83de145defa806e62be7529f25825ad845e0
Dolloties <101804337+Dolloties@users.noreply.github.com>
2023-02-13 09:04:22 +0100

    hit detection

--- cube.gd
+++ cube.gd
@@ -25,3 +25,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Area_body_entered(body):
+	print("i hit something?", body)
+	queue_free() # Replace with function body.
+
+
+func _on_Area2_body_entered2(body):
+	print("i hit something2?", body)
+	queue_free() # Replace with function body.
